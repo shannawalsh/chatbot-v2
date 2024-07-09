fef5b8bfcf2358d6c62ae2b9a966572704b4bd0f
Shanna <shanna.r.walsh@gmail.com>
2024-07-09 18:59:23 -0500

    token input limit added

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 
 client = OpenAI()
+import tiktoken
 
 # accepts a preferred model and a list of messages
 # makes chat completions API call
@@ -9,9 +10,8 @@ def get_api_chat_response_message(model, messages):
     # make the API call
     api_response = client.chat.completions.create(
         model = model,
-        messages = messages
+        messages = messages,
     )
-
     # extract the response text
     response_content = api_response.choices[0].message.content
 
@@ -19,6 +19,8 @@ def get_api_chat_response_message(model, messages):
     return response_content
 
 model = "gpt-3.5-turbo"
+encoding = tiktoken.encoding_for_model(model)
+print(encoding)
 
 chat_history = []
 
@@ -26,6 +28,16 @@ while True:
     user_input = input("You: ")
     if user_input.lower() == "exit":
         break
+    
+    #user_input_encoded = encoding.encode(user_input)
+    #print(user_input_encoded)
+    token_count = len(encoding.encode(user_input))
+    token_input_limit = 12289
+    #print(token_count)
+    
+    if (token_count > token_input_limit):
+        print("Your prompt is too long. Please try again.")
+        continue
     
     chat_history.append({
         "role": "user",
